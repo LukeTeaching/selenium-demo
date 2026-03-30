3af9570b9a538b19cf598fd97c85040a35b5ea7b
Luke <npv.loc@gmail.com>
2026-03-30 21:00:10 +0700

    Add POM architect, TDD

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumTests.Pages;
+
+public class LoginPage
+{
+    private readonly IWebDriver _driver;
+
+    // 1. Constructor: Nhận driver từ lớp Test truyền vào
+    public LoginPage(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+
+    // 2. Locators: Định vị các phần tử UI (Để private để đóng gói tính năng)
+    private readonly By _usernameInput = By.Id("username");
+    private readonly By _passwordInput = By.Id("password");
+    private readonly By _loginButton = By.CssSelector("button[type='submit']");
+    private readonly By _flashMessage = By.Id("flash");
+
+    // 3. Methods: Các hành vi mà user có thể làm trên trang này
+    public void GoTo()
+    {
+        _driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+    }
+
+    // Gom thao tác đăng nhập thành một hàm tiện ích
+    public void LoginAs(string username, string password)
+    {
+        _driver.FindElement(_usernameInput).SendKeys(username);
+        _driver.FindElement(_passwordInput).SendKeys(password);
+        _driver.FindElement(_loginButton).Click();
+    }
+
+    // Hàm lấy thông báo để phục vụ cho việc Assert ở tầng Test
+    public string GetFlashMessage()
+    {
+        return _driver.FindElement(_flashMessage).Text;
+    }
+}
---
+++ Tests/LoginTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using SeleniumTests.Pages; // Import thư mục Pages
+
+namespace SeleniumTests.Tests;
+
+// Kế thừa BaseTest để tự động chạy Setup() và TearDown()
+public class LoginTests : BaseTest
+{
+    [Test]
+    public void Test_ValidLogin_ShouldSucceed()
+    {
+        // 1. Arrange: Khởi tạo trang Login
+        var loginPage = new LoginPage(driver);
+
+        // 2. Act: Thực hiện các hành vi
+        loginPage.GoTo();
+        loginPage.LoginAs("tomsmith", "SuperSecretPassword!");
+
+        // 3. Assert: Kiểm tra kết quả
+        var actualMessage = loginPage.GetFlashMessage();
+        Assert.That(actualMessage, Does.Contain("You logged into a secure area!"));
+    }
+
+    [Test]
+    public void Test_InvalidLogin_ShouldFail()
+    {
+        var loginPage = new LoginPage(driver);
+
+        loginPage.GoTo();
+        loginPage.LoginAs("wronguser", "wrongpassword");
+
+        // Tận dụng lại hàm GetFlashMessage để test case thất bại
+        var actualMessage = loginPage.GetFlashMessage();
+        Assert.That(actualMessage, Does.Contain("Your username is invalid!"));
+    }
+}
